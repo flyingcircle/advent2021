3318db535092ec960fc8591c8b7c8ccf86395f97
Jeremy Hamilton <jeremy.hamilton@tekfive.com>
2021-12-06 22:00:01 -0800

    solution to day 5

---
+++ 5/part1/vents.hs
@@ -0,0 +1,50 @@
+import Data.Map (fromListWith, toList)
+
+type Coords = (Int, Int, Int, Int)
+
+type Coord = (Int, Int)
+
+type Board = [[Int]]
+
+main :: IO ()
+main = do
+  f <- readFile "../inputs.txt"
+  let inputs = lines f
+  let vents = (filterDiagonalVents . parseCoords . map words) inputs
+  let placedVents = frequency (concatMap calcCoords vents)
+  print ("Num of overlaps: " ++ show (countOverlap placedVents))
+
+parseCoords :: [[String]] -> [Coords]
+parseCoords = map parseCoord
+
+parseCoord :: [String] -> Coords
+parseCoord [c1,_,c2] = 
+  let [x1, y1] = wordsWhen (==',') c1 in
+    let [x2, y2] = wordsWhen (==',') c2 in
+      (read x1, read y1, read x2, read y2)
+parseCoord _ = (0,0,0,0)
+
+filterDiagonalVents :: [Coords] -> [Coords]
+filterDiagonalVents = filter (\(x1,y1,x2,y2) -> x1 == x2 || y1 == y2)
+
+calcCoords :: Coords -> [Coord]
+calcCoords (x1,y1,x2,y2)
+  | x1 == x2 = let ys = if y2 > y1 then [y1..y2] else [y2..y1] in [(x1,y3) | y3 <- ys]
+  | y1 == y2 = let xs = if x2 > x1 then [x1..x2] else [x2..x1] in [(x3,y1) | x3 <- xs]
+calcCoords (_,_,_,_) = []
+
+
+doesCoordOverlap :: (Coord,Int) -> Bool
+doesCoordOverlap (_,i) = i > 1
+
+countOverlap :: [(Coord,Int)] -> Int
+countOverlap = length . filter doesCoordOverlap
+
+frequency :: (Ord a) => [a] -> [(a, Int)]
+frequency xs = toList (fromListWith (+) [(x, 1) | x <- xs])
+
+wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s = case dropWhile p s of
+                      "" -> []
+                      s' -> w : wordsWhen p s''
+                            where (w, s'') = break p s'
